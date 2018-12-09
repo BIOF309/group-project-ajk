4b4892cd4dcad5790e9886382447e6b72f4e52ac
kimalj <ajk5mq@virginia.edu>
2018-12-09 05:15:04 -0500

    Creates data analysis of NCHS csv file

--- Data Processing.py
+++ Data Processing.py
@@ -1,30 +1,31 @@
 # import packages
 
 import pandas as pd
-import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
 
 # import excel file and create dataframe called NCHS
 
-df = 'NCHS_-_Leading_Causes_of_Death__United_States.csv'
-NCHS = pd.read_csv(df)
+NCHS = pd.read_csv('NCHS_-_Leading_Causes_of_Death__United_States.csv')
 
-# view NCHS columns
-NCHS_columns1 = pd.read_csv(df, index_col=0)
-print(NCHS_columns1.head(10296))
+# examine the data
 
-# view number of rows and columns
-NCHS_columns1.shape
-print(NCHS_columns1.shape)
+NCHS.info()
 
-# Pie chart for distribution by activity code
-colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral','red','green','blue','orange','white','brown']
-NCHS['Actv'].value_counts().plot(kind='pie',title='Activity',colors=colors)
+year = NCHS[['Cause Name', 'Year', 'Deaths']]
+
+year_group = year.groupby(['Year','Cause Name'])
+year_group_total = year_group.sum()
+
+print(year_group_total)
+
+print(year_group_total.unstack())
+
+barplot = year_group_total.unstack().plot(kind='bar',stacked=True,title='Total Deaths by Year',figsize=(9,7))
+barplot.set_xlabel('Year')
+barplot.set_ylabel('Deaths')
+barplot.set_ylim((0,10000000))
+barplot.legend(["All causes","Alzheimer's disease","CLRD","Cancer", "Diabetes","Heart disease", "Influenza and pneumonia", "Kidney disease", "Stroke", "Suicide", "Unintentional injuries"])
+
 plt.show()
-
-
-
-
-
 
